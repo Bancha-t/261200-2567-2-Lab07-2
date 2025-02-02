5b3d717eea7b66d8d6f6b78c0f45096bab40527a
Bancha-t <gmtv7887@gmail.com>
2025-02-03 03:16:59 +0700

    updataClassCharacter

---
+++ Character.java
@@ -0,0 +1,47 @@
+public class Character {
+    private int health;
+    private int stamina;
+    private final int maxHealth;
+    private final int maxStamina;
+
+    Character(int maxHealth, int maxStamina){
+        this.health = maxHealth;
+        this.stamina = maxStamina;
+        this.maxHealth = maxHealth;
+        this.maxStamina = maxStamina;
+        System.out.println("Character created with " + maxHealth + " health and " + maxStamina + " stamina.");
+    }
+
+    public int getHealth(){
+        return health;
+    }
+
+    public int getStamina(){
+        return stamina;
+    }
+
+    public void swingSword(){
+        int staminaCost = 10;
+        if(stamina >= staminaCost){
+            stamina -= staminaCost;
+            System.out.println("Character swings sword. Stamina is now " + getStamina() + ".");
+        }
+        else{
+            System.out.println("Your character needs to cool down.");
+        }
+    }
+
+    public void takeDamage(int damage){
+        health -= damage;
+        System.out.println("Character takes " + damage + " damage. Health is now " + getHealth() + ".");
+        if(health == 0){
+            System.out.println("Character is dead.");
+        }
+    }
+
+    public void rest(){
+        health = maxHealth;
+        stamina = maxStamina;
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        Character hero = new Character(100, 50);
+
+        hero.swingSword();
+        hero.swingSword();
+        hero.swingSword();
+        hero.swingSword();
+        hero.swingSword();
+        hero.swingSword();
+
+        hero.takeDamage(30);
+        hero.takeDamage(50);
+        hero.takeDamage(20);
+
+        hero.rest();
+
+        System.out.println("Character's health: " + hero.getHealth() + ", stamina: " + hero.getStamina() + ".");
+    }
+}
